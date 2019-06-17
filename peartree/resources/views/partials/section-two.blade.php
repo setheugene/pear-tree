1fd60864eb8b18ee6652fd929e422846e475afb6
setheugene <seth.eugene@icloud.com>
2019-06-16 20:05:37 -0500

    acf working

--- peartree/resources/views/partials/section-two.blade.php
+++ peartree/resources/views/partials/section-two.blade.php
@@ -1,8 +1,7 @@
 @section('content')@endsection
 
-
 <div class="container text-center">
-<div class="heading-sub">a spread for any event</div>
-<p class="script">Pear Tree Catering has been serving the Chicagoland community for over 25 years. We are committed to offering freshly prepared gourmet foods and ingredients with excellent service to all of our clients. We provide a variety of services and have everything needed to suit catering needs for parties of 2-2,000. At Pear Tree Catering we strive to not only create delicious food, but also create an ambiance and night to remember for all of our guests. We can take care of everything from table decorations and themed centerpieces to costumed servers, anything we can do to ensure a dynamic and memorable occasion for our clients.</p>
+<div class="heading-sub"><?php the_field('header'); ?></div>
+<p class="script"><?php the_field('paragraph'); ?></p>
 <a href="#" class="btn btn-green">About Us</a>
 </div>
